b99120b7777e5741f63b8787f7c3ca5e2ecf0644
poowoo90 <poowoo90@gmail.com>
2018-01-26 11:04:24 +0900

    저장

--- app/src/main/assets/common.js
+++ app/src/main/assets/common.js
@@ -11,21 +11,12 @@ function onSubmit() {
     var result_value = $("#a1").val();
     android.receive(result_value);
 
-    //window.android.sendMessage();
     window.android.movePage();
 }
-/*
+
+
 function receive(arg) {
-/*
-    //var receive_result = "";
-    var receive_result = android.sendMessage();
-    //$("#value1").val(receive_result);
-    alert(receive_result);
-    document.getElementById('value1').innerHTML = receive_result;
-
-    $("#value1").val(arg);
-
-    alert(arg);
+    return arg;
 }
-*/
+
 
