cbfd1692f49351ccacd80908a7d630bcc6bbed18
poowoo90 <poowoo90@gmail.com>
2018-01-30 10:34:55 +0900

    저장

--- app/src/main/assets/common.js
+++ app/src/main/assets/common.js
@@ -1,22 +1,201 @@
+//----------------------------------------------------------------------------------------------------------------------------------------//
+// 공통변수
+//----------------------------------------------------------------------------------------------------------------------------------------//
+/**
+대출계좌          : loanAccount
+약정금액          : loanLimitAmt
+신용정보평가 금액 : CBAmt
+통신정보평가 금액 : NBAmt
+약정금리          : contractRate
+신용정보평가 금리 : CBRate
+통신정보평가 금리 : NBRate
+직장명            : officeNm
+연소득            : annualIncome
+연체여부          : arrearsYN
+연체금액          : arrearsAmt
+**/
+
+var SUZAN = {
+			"phone":"01037890032",
+			"loanAccount":"111222333333",
+			"conAmt":10000,
+			"CBAmt":0,
+			"NBAmt":10000,
+			"contractRate":3.0,
+			"CBRate":0,
+			"NBRate":3.0,
+			"officeNm":"기아자동차",
+			"annualIncome":40000,
+			"arrearsYN":"N",
+			"arrearsAmt":0
+			};
+
+var AMIE  = {
+			"phone":"01027333486",
+			"loanAccount":"111222333333",
+			"conAmt":150000,
+			"CBAmt":100000,
+			"NBAmt":50000,
+			"contractRate":1.3,
+			"CBRate":2.5,
+			"NBRate":1.2,
+			"officeNm":"기아자동차",
+			"annualIncome":40000,
+			"arrearsYN":"N",
+			"arrearsAmt":0
+			};
+
+
+var TOM   = {
+			"phone":"01011563932",
+			"loanAccount":"111222333333",
+			"conAmt":80000,
+			"CBAmt":60000,
+			"NBAmt":20000,
+			"contractRate":2.5,
+			"CBRate":3.5,
+			"NBRate":1.0,
+			"officeNm":"기아자동차",
+			"annualIncome":40000,
+			"arrearsYN":"N",
+			"arrearsAmt":0
+			};
+
+var BRIAN = {
+			"phone":"01066324486",
+			"loanAccount":"111222333333",
+			"conAmt":15000,
+			"CBAmt":0,
+			"NBAmt":15000,
+			"contractRate":4.1,
+			"CBRate":0,
+			"NBRate":4.1,
+			"officeNm":"기아자동차",
+			"annualIncome":40000,
+			"arrearsYN":"N",
+			"arrearsAmt":0
+			};
+
+var JADE  = {
+			"phone":"01078662469",
+			"loanAccount":"111222333333",
+			"conAmt":60000,
+			"CBAmt":0,
+			"NBAmt":60000,
+			"contractRate":2.1,
+			"CBRate":0,
+			"NBRate":2.1,
+			"officeNm":"기아자동차",
+			"annualIncome":40000,
+			"arrearsYN":"N",
+			"arrearsAmt":0
+			};
+
+
+var JOHN  = {
+			"phone":"01044223348",
+			"loanAccount":"111222333333",
+			"conAmt":5000,
+			"CBAmt":0,
+			"NBAmt":5000,
+			"contractRate":3.6,
+			"CBRate":0,
+			"NBRate":3.6,
+			"officeNm":"기아자동차",
+			"annualIncome":40000,
+			"arrearsYN":"N",
+			"arrearsAmt":0
+			};
+
+var CAL  = {
+			"phone":"01011112222",
+			"loanAccount":"111222333333",
+			"conAmt":100000,
+			"CBAmt":90000,
+			"NBAmt":10000,
+			"contractRate":3.0,
+			"CBRate":3.6,
+			"NBRate":0.6,
+			"officeNm":"기아자동차",
+			"annualIncome":40000,
+			"arrearsYN":"N",
+			"arrearsAmt":0
+			};
+
+/* 비활성화 메뉴 토스트메세지 보이기 */
 
 function showAndroidToast(toast) {
 			android.showToast(toast);
 }
 
-function pushCancel() {
+/* 취소 버튼에 대한 처리 */
+function common_Cancel() {
     android.onCancelPressed();
 }
 
-function onSubmit() {
+/* 뒤로가기 버튼에 대한 처리 */
+function common_goBack() {
+    window.history.back();
+}
+
+/* 안드로이드에서 데이터 값 받아오기 */
+function common_getJsonData() {
+    window.android.getJsonData();
+}
+
+/* 페이지 이동, 데이터 보내기 */
+function common_movePage(url, jsonData) {
+    if(jsonData != null) {
+    var name = jsonData.name;
+        window.android.movePage(url, JSON.stringify(jsonData));
+    } else {
+        window.android.movePage(url, null);
+    }
+}
+
+/* 해당하는 사용자 정보 보내기 */
+function common_getUserData(name) {
+    //alert("name : " + name);
+    name = name.toUpperCase();
+    if(name == 'SUZAN') {
+        return SUZAN;
+    } else if(name == 'AMIE') {
+        return AMIE;
+    } else if(name == 'TOM') {
+        return TOM;
+    } else if(name == 'BRIAN') {
+        return BRIAN;
+    } else if(name == 'JADE') {
+        return JADE;
+    } else if(name == 'JOHN') {
+        return JOHN;
+    } else {
+        return CAL;
+    }
+}
+
+//----------------------------------------------------------------------------------------------------------------------------------------//
+// TEST 용 함수들
+//----------------------------------------------------------------------------------------------------------------------------------------//
+
+/*
+
+function pushCancel(arg) {
+    //alert(arg);
+    android.onCancelPressed(arg);
+}
+
+function onSubmit(url) {
     var result_value = $("#a1").val();
     android.receive(result_value);
 
-    window.android.movePage();
+    window.android.movePage(url);
 }
 
 
-function receive(arg) {
-    return arg;
+function movePage(url) {
+    window.android.movePage(url);
 }
 
+*/
 
--- app/src/main/java/com/example/nam/kcb_demo/MainActivity.java
+++ app/src/main/java/com/example/nam/kcb_demo/MainActivity.java
@@ -3,8 +3,6 @@ package com.example.nam.kcb_demo;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.DialogInterface;
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Build;
 import android.os.Handler;
 import android.support.annotation.RequiresApi;
@@ -21,12 +19,19 @@ import android.webkit.WebViewClient;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
+import org.json.JSONException;
+import org.json.JSONObject;
 
 
 public class MainActivity extends AppCompatActivity {
-    WebView web; //웹뷰 선언
-    String receive = "";
+    WebView web;            //웹뷰 선언
+    JSONObject jsonData; // 자바스크립트에서 값을 받을 json 변수 선언
+
+    /* 테스트용 변수 */
+    // String receive = "";   // 자바스크립트에서 값을 받을 변수 선언
+
     private final Handler handler = new Handler();
+
 
     @SuppressLint("WrongConstant")
     @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
@@ -46,14 +51,14 @@ public class MainActivity extends AppCompatActivity {
         webSet.setUseWideViewPort                       (true) ; // 웹뷰에 맞게 출력하기
         webSet.setBuiltInZoomControls                   (false); // 안드로이드 내장 줌 컨트롤 사용 X
         webSet.setAllowUniversalAccessFromFileURLs      (true) ; // file://URL이면 어느 오리진에 대해서도 Ajax로 요청을 보낼 수 있다.
-        // API 레벨 16부터 이용할 수 있다.
+                                                                 // API 레벨 16부터 이용할 수 있다.
         webSet.setJavaScriptCanOpenWindowsAutomatically (true) ; // javascript 가  window.open()을 사용할 수 있도록 설정
         webSet.setSupportMultipleWindows                (true) ; // 여러개의 윈도우를 사용할 수 있도록 설정
         webSet.setSaveFormData                          (false); // 폼의 입력값를 저장하지 않는다
         webSet.setSavePassword                          (false); // 암호를 저장하지 않는다.
         webSet.setLayoutAlgorithm                       (WebSettings.LayoutAlgorithm.SINGLE_COLUMN); // 컨텐츠 사이즈 맞추기
 
-        web.addJavascriptInterface(new WebAppInterface(this), "android");
+        web.addJavascriptInterface(new WebAppInterface(this), "android"); // 자바스크립트 인터페이스 (이름을 android 로 설정)
 
 
         web.setWebChromeClient(new WebChromeClient());
@@ -66,16 +71,20 @@ public class MainActivity extends AppCompatActivity {
             }
 
         });
-        web.loadUrl("file:///android_asset/index.html");
+
+        web.loadUrl("file:///android_asset/1001.html"); // 처음 로드할 페이지
         layout.addView(web);
         setContentView(layout);
 
 
     }
 
+    /* 물리 백버튼키 처리 */
     @Override
     public void onBackPressed() {
         WebBackForwardList list = web.copyBackForwardList(); // 누적된 history 를 저장할 변수
+        //int listSize = list.getSize(); // list의 크기
+        //web.goBackOrForward(-(list.getCurrentIndex())); // 현재 페이지로 부터 history 수 만큼 뒷 페이지로 이동
 
         Log.d("[JeongjinKim]", "onBackPressed: During");
         if (list.getCurrentIndex() <= 0 && !web.canGoBack()) { // 처음 들어온 페이지이거나, history 가 없는 경우
@@ -96,17 +105,18 @@ public class MainActivity extends AppCompatActivity {
                     .setNegativeButton("아니오", null)
                     .show();
 
-        }else { // history 가 있는 경우
+        } else { // history 가 있는 경우
 
-            if(web.canGoBack()){
+            if (web.canGoBack()) {
                 web.goBack();
             }
+
             web.clearHistory(); // history 삭제
 
         }
     }
 
-    /* 안드로이드와 html간의 데이터 주고 받기 */
+    /* 안드로이드와 자바스크립트간의 데이터 주고 받기 */
     public class WebAppInterface {
         Context mContext;
 
@@ -123,35 +133,101 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(mContext, toast, Toast.LENGTH_SHORT).show();
         }
 
+        /* 뒤로가기 */
         @JavascriptInterface
         public void onCancelPressed() {
             Log.d("[JeongjinKim]", "onCancelPressed: start ");
             web.post(new Runnable() {
                 @Override
                 public void run() {
-                    onBackPressed();
+                    WebBackForwardList list = web.copyBackForwardList(); // 누적된 history 를 저장할 변수
+                    web.goBackOrForward(-(list.getCurrentIndex()) + 1);
                 }
             });
             Log.d("[JeongjinKim]", "onCancelPressed: end ");
         }
 
+        /* 페이지 이동, 자바스크립트에서 데이터받기 */
         @JavascriptInterface
-        public void receive(String arg) {
-            receive = arg;
-            String url = "file:///android_asset/index.html";
-        }
-
-
-        @JavascriptInterface
-        public void movePage() {
+        public void movePage(final String url, final String json) {
             handler.post(new Runnable() {
                 @Override
                 public void run() {
-                    web.loadUrl("file:///android_asset/sub2.html"); // url로 페이지 이동
+                    //web.loadUrl("file:///android_asset/sub2.html"); // url로 페이지 이동
+                    if(json != null) {
+                        try {
+                            jsonData = new JSONObject(json);
+                            //Toast.makeText(mContext, json, Toast.LENGTH_SHORT).show();
+                        }catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    web.loadUrl("file:///android_asset/" + url); // url로 페이지 이동
                 }
             });
         }
 
+        /* 자바스크립트 함수로 데이터 전송 */
+        @JavascriptInterface
+        public void getJsonData() {
+            handler.post(new Runnable() {
+                @Override
+                public void run() {
+                    web.loadUrl("javascript:onCreate('" + jsonData.toString() + "')"); // 해당 url의 자바스크립트 함수 호출
+                }
+            });
+        }
+
+        //----------------------------------------------------------------------------------------------------------------------------------------//
+        // TEST 용 함수들
+        //----------------------------------------------------------------------------------------------------------------------------------------//
+
+        /*
+
+        /* 뒤로가기
+        @JavascriptInterface
+        public void onCancelPressed(final String args) {
+            Log.d("[JeongjinKim]", "onCancelPressed: start ");
+            web.post(new Runnable() {
+                @Override
+                public void run() {
+                    WebBackForwardList list = web.copyBackForwardList(); // 누적된 history 를 저장할 변수
+                    Log.d("NGW", "args = [ " + args + " ]");
+                    if(args.equals("Y")){
+                        web.goBackOrForward(-(list.getCurrentIndex()) + 1);
+                    }else if(args.equals("N")) {
+                        if (web.canGoBack()) {
+                            web.goBack();
+                        }
+                    }
+                    web.clearHistory(); // history 삭제
+                }
+            });
+            Log.d("[JeongjinKim]", "onCancelPressed: end ");
+        }
+
+
+        /* 자바스크립트에서 전송된 데이터 받기
+        @JavascriptInterface
+        public void receive(String arg) {
+            receive = arg;      // 자바스크립트에서 받은 데이터
+            //String url = "file:///android_asset/index.html";
+        }
+
+
+        /* 페이지 이동
+        @JavascriptInterface
+        public void movePage(final String receiveUrl) {
+            handler.post(new Runnable() {
+                @Override
+                public void run() {
+                    //web.loadUrl("file:///android_asset/sub2.html"); // url로 페이지 이동
+                    web.loadUrl(receiveUrl); // url로 페이지 이동
+                }
+            });
+        }
+
+        /* 자바스크립트 함수로 데이터 전송
         @JavascriptInterface
         public void sendMessage() {
             handler.post(new Runnable() {
@@ -161,6 +237,10 @@ public class MainActivity extends AppCompatActivity {
                 }
             });
         }
+
+         */
+
+
     }
 
 
