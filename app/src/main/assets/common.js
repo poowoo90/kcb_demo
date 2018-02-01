a9b9ef5d7217a0d016bc8e0909e483e650f8e195
HwangYuJeong <hyjlm92@gmail.com>
2018-02-01 17:39:40 +0900

    ~.~

--- app/src/main/assets/common.js
+++ app/src/main/assets/common.js
@@ -234,3 +234,20 @@ function Test_onSubmit() {
     android.receive(result_value);
     window.android.movePage();
 }
+
+
+
+//----------------------------------------------------------------------------------------------------------------------------------------//
+// 메뉴 슬라이드
+//----------------------------------------------------------------------------------------------------------------------------------------//
+function openNav() {
+   /* document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";*/
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+    document.body.style.backgroundColor = "white";
+}
